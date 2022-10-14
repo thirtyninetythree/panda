b1adc8e315a5e9b90a358590bfda6cdd650f50f2
Karanja <98053458+thirtyninetythree@users.noreply.github.com>
2022-10-14 17:31:34 +0300

    added map, removed some material ui

--- App.js
+++ App.js
@@ -1,36 +1,53 @@
-import "./style.css"
+import './style.css'
+
 import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import Grid from '@mui/material/Unstable_Grid2'
+
+import { TypeAnimation } from 'react-type-animation';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
 
 function App() {
+  new Map({
+    layers: [
+      new TileLayer({source: new OSM()}),
+    ],
+    view: new View({
+      center: [0, 0],
+      zoom: 2,
+    }),
+    target: 'map',
+  })
+  
   return (
     <div className="App">
-        <body>
-      <Box sx={{ width: '100%', maxWidth: 500 }}>
-        <Typography variant="h1" gutterBottom>
-          panda.
-        </Typography>
-        <Typography variant="h3" gutterBottom>
-          ReFi for the your farm.
-        </Typography>
-      </Box>
+     
+        <Box sx={{ width: '100%', maxWidth: 500 }}>
+          <h1>panda.</h1>
+         
+          <TypeAnimation
+            // Same String at the start will only be typed once, initially
+            sequence={[
+              'ReFi for the your farm.',
+              1000,
+              'Monetize your garden',
+              1000,
+              'Incentives for afforestation',
+              1000,
+              'Save the world',
+              1000,
+            ]}
+            speed={50} // Custom Speed from 1-99 - Default Speed: 40
+            style={{ fontSize: '2em' }}
+            wrapper="span" // Animation will be rendered as a <span>
+            repeat={Infinity} // Repeat this Animation Sequence infinitely
+          />
+        </Box>
+        
 
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid xs={8}>
-            <Typography variant="h1" gutterBottom>
-              Map. W
-            </Typography>
-          </Grid>
-          <Grid xs={4}>
-            <Typography variant="h1" gutterBottom>
-              Stats.
-            </Typography>
-          </Grid>
-        </Grid>
-      </Box>
-      </body>
+        <div id="map"></div>
+        
+      
     </div>
   )
 }

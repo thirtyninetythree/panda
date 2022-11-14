aa91a82921340608e365d74e19b4410b13ff1ac0
Karanja <98053458+thirtyninetythree@users.noreply.github.com>
2022-11-14 19:02:14 +0300

    added working chainlink call to nvri

--- refi.sol
+++ refi.sol
@@ -9,10 +9,12 @@ contract ReFi is ChainlinkClient, ConfirmedOwner {
     using Chainlink for Chainlink.Request;
 
     uint256 public volume;
+    uint256 public nvri;
+    uint256 public treeCoverScore;
     bytes32 private jobId;
     uint256 private fee;
 
-    event RequestVolume(bytes32 indexed requestId, uint256 volume);
+    event RequestNVRI(bytes32 indexed requestId, uint256 nvri);
 
     /**
      * @notice Initialize the link token and target oracle
@@ -24,7 +26,8 @@ contract ReFi is ChainlinkClient, ConfirmedOwner {
      *
      */
     constructor() ConfirmedOwner(msg.sender) {
-        setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+        // fantom testnet 
+        setChainlinkToken(0xfaFedb041c0DD4fA2Dc0d87a6B0979Ee6FA7af5F);
         setChainlinkOracle(0xCC79157eb46F5624204f47AB42b3906cAA40eaB7);
         jobId = 'ca98366cc7314957b8c012c72f05aeeb';
         fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
@@ -34,24 +37,18 @@ contract ReFi is ChainlinkClient, ConfirmedOwner {
      * Create a Chainlink request to retrieve API response, find the target
      * data, then multiply by 1000000000000000000 (to remove decimal places from data).
      */
-    function requestVolumeData() public returns (bytes32 requestId) {
+    /**
+     * Create a Chainlink request to retrieve API response, find the target
+     * data, then multiply by 1000000000000000000 (to remove decimal places from data).
+     */
+    function requestNVRI() public returns (bytes32 requestId) {
         Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
 
         // Set the URL to perform the GET request on
-        req.add('get', 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD');
+        req.add('get', 'http://api.agromonitoring.com/agro/1.0/ndvi/history?start=1530336000&end=1534976000&polyid=6370ef3e5c2911000750f13c&appid=6e9140f0770d3c84709cd033b2918192');
 
-        // Set the path to find the desired data in the API response, where the response format is:
-        // {"RAW":
-        //   {"ETH":
-        //    {"USD":
-        //     {
-        //      "VOLUME24HOUR": xxx.xxx,
-        //     }
-        //    }
-        //   }
-        //  }
-        // request.add("path", "RAW.ETH.USD.VOLUME24HOUR"); // Chainlink nodes prior to 1.0.0 support this format
-        req.add('path', 'RAW,ETH,USD,VOLUME24HOUR'); // Chainlink nodes 1.0.0 and later support this format
+       
+        req.add('path', '0,data,mean'); // Chainlink nodes 1.0.0 and later support this format
 
         // Multiply the result by 1000000000000000000 to remove decimals
         int256 timesAmount = 10**18;
@@ -60,20 +57,18 @@ contract ReFi is ChainlinkClient, ConfirmedOwner {
         // Sends the request
         return sendChainlinkRequest(req, fee);
     }
-    /**
-     * Create a Chainlink request to retrieve API response, find the target
-     * data, then multiply by 1000000000000000000 (to remove decimal places from data).
-     */
-    function getNVRI() {
-
-    }
 
     /**
      * Receive the response in the form of uint256
      */
-    function fulfill(bytes32 _requestId, uint256 _volume) public recordChainlinkFulfillment(_requestId) {
-        emit RequestVolume(_requestId, _volume);
-        volume = _volume;
+    // function fulfill(bytes32 _requestId, uint256 _volume) public recordChainlinkFulfillment(_requestId) {
+    //     emit RequestVolume(_requestId, _volume);
+    //     volume = _volume;
+    // }
+
+    function fulfill(bytes32 _requestId, uint256 _nvri) public recordChainlinkFulfillment(_requestId) {
+        emit RequestNVRI(_requestId, _nvri);
+        nvri = _nvri;
     }
 
     /**
@@ -83,7 +78,4 @@ contract ReFi is ChainlinkClient, ConfirmedOwner {
         LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
         require(link.transfer(msg.sender, link.balanceOf(address(this))), 'Unable to transfer');
     }
-
-
-
 }

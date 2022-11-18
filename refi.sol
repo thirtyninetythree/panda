b8586c85e971e9246ebbb763236d40213ed7c0d0
Karanja <98053458+thirtyninetythree@users.noreply.github.com>
2022-11-18 18:22:40 +0300

    update refi.sol

--- refi.sol
+++ refi.sol
@@ -10,21 +10,13 @@ contract ReFi is ChainlinkClient, ConfirmedOwner {
 
     uint256 public volume;
     uint256 public nvri;
-    uint256 public treeCoverScore;
+    uint256 public areaInHectares;
+    uint256 public carbonCredits;
     bytes32 private jobId;
     uint256 private fee;
 
     event RequestNVRI(bytes32 indexed requestId, uint256 nvri);
 
-    /**
-     * @notice Initialize the link token and target oracle
-     *
-     * Goerli Testnet details:
-     * Link Token: 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
-     * Oracle: 0xCC79157eb46F5624204f47AB42b3906cAA40eaB7 (Chainlink DevRel)
-     * jobId: ca98366cc7314957b8c012c72f05aeeb
-     *
-     */
     constructor() ConfirmedOwner(msg.sender) {
         // fantom testnet 
         setChainlinkToken(0xfaFedb041c0DD4fA2Dc0d87a6B0979Ee6FA7af5F);
@@ -37,15 +29,11 @@ contract ReFi is ChainlinkClient, ConfirmedOwner {
      * Create a Chainlink request to retrieve API response, find the target
      * data, then multiply by 1000000000000000000 (to remove decimal places from data).
      */
-    /**
-     * Create a Chainlink request to retrieve API response, find the target
-     * data, then multiply by 1000000000000000000 (to remove decimal places from data).
-     */
     function requestNVRI() public returns (bytes32 requestId) {
         Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
 
         // Set the URL to perform the GET request on
-        req.add('get', 'http://api.agromonitoring.com/agro/1.0/ndvi/history?start=1530336000&end=1534976000&polyid=6370ef3e5c2911000750f13c&appid=6e9140f0770d3c84709cd033b2918192');
+        req.add('get', 'http://api.agromonitoring.com/agro/1.0/ndvi/history?start=1530336000&end=1534976000&polyid=6370ef3e5c2911000750f13c&appid=<YOUR_API_KEY>');
 
        
         req.add('path', '0,data,mean'); // Chainlink nodes 1.0.0 and later support this format
@@ -58,14 +46,6 @@ contract ReFi is ChainlinkClient, ConfirmedOwner {
         return sendChainlinkRequest(req, fee);
     }
 
-    /**
-     * Receive the response in the form of uint256
-     */
-    // function fulfill(bytes32 _requestId, uint256 _volume) public recordChainlinkFulfillment(_requestId) {
-    //     emit RequestVolume(_requestId, _volume);
-    //     volume = _volume;
-    // }
-
     function fulfill(bytes32 _requestId, uint256 _nvri) public recordChainlinkFulfillment(_requestId) {
         emit RequestNVRI(_requestId, _nvri);
         nvri = _nvri;
@@ -77,5 +57,19 @@ contract ReFi is ChainlinkClient, ConfirmedOwner {
     function withdrawLink() public onlyOwner {
         LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
         require(link.transfer(msg.sender, link.balanceOf(address(this))), 'Unable to transfer');
+    }
+
+    function setAreaInHectares(uint256 _area) public {
+        areaInHectares = _area;
+    }
+
+    function calculateCarbonCreditFromNVRI() public {
+        carbonCredits = ( nvri / 10 ** 18 ) * areaInHectares * 11 * 600;
+    }
+    function getNVRI() public view returns(uint256) {
+        return nvri;
+    }
+     function getCarbonCreditsAmount() public view returns(uint256) {
+        return nvri;
     }
 }
